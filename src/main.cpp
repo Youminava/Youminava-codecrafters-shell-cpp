f7818c4dab5858c58e884895b1636e16519f111f
Youminava <kirill.2301@bk.ru>
2025-10-31 21:28:23 +0300

    pervoe zadanie

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,10 @@ using namespace std;
 
 int main() {
     cout << unitbuf;
-    cerr << unitbuf;
     
-    while(true) {
-        cout << "$ ";
-        string input;
-        
-        if(!getline(cin, input)) {
-            break;
-        }
-
-        if (input == "\\q") {
-            cout << "Exit" << endl;
-            break;
-        }
-
-        if (input.find("echo ") == 0) {
-            string message = input.substr(5);
-            cout << message << endl;
-        }
-        else if (!input.empty()) {
-            cout << "ERROR" << endl;
-        }
+    string input;
+    if(getline(cin, input)) {
+        cout << input << endl;
     }
     
     return 0;
