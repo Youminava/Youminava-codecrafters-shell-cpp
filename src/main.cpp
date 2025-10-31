2d2d98c0c4607128ab1619377910484cc02389da
Youminava <kirill.2301@bk.ru>
2025-10-31 23:20:10 +0300

    CHETVERTOE I PYATOE ZADANIE

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
 int main() {
-    cout << unitbuf;
-    
+    cout << unitbuf;    
     string input;
-    while(getline(cin, input)) {
+   
+    string history_file = "/home/govno/kubsh_history";
+
+    while(true) {
+     cout << "$ ";
+    if(!getline(cin, input)) break; 
+   
+    if(!input.empty() && input != "\\q" && input != "history") {
+    ofstream file(history_file, ios::app);
+    file << input << endl;
+    }
+
      if(input == "\\q") {return 0;}
-   cout << input << endl;
- }
+     else if(input.find("echo ") == 0) {
+     string message = input.substr(5);
+     cout << message << endl;
+   } 
+    else if(input == "history") {
+    ifstream read_file(history_file);
+    string line;
+    while(getline(read_file, line)) {
+        cout << " " << line << endl;
+      }
+  }
+ else cout << input << endl;
+}
 }
