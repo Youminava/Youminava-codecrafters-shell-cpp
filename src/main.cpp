29f3459db1134c66dfac67327ab05ecb2eac80f3
Youminava <kirill.2301@bk.ru>
2025-11-23 20:50:01 +0300

    CI/CD

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <unistd.h>
 using namespace std;
 
 int main() {
@@ -16,15 +18,26 @@ int main() {
     else history_file = "/kubsh_history";
 
     while(true) {
-    cout << "$ ";
+    if (isatty(STDIN_FILENO)) {
+        cout << "$ ";
+    }
     if(!getline(cin, input)) break; 
     if(!input.empty()) {
       bool isValid = false;
-      if(input == "\\q" || input == "history") isValid = true;
-      else if(input.find("echo ") == 0 || input == "echo") isValid = true;
+      string cmdName = input;
+      size_t spacePos = input.find(' ');
+      if(spacePos != string::npos) {
+        cmdName = input.substr(0, spacePos);
+      }
+
+      if(cmdName == "\\q" || cmdName == "history" || cmdName == "help" || 
+         cmdName == "exit" || cmdName == "type" || cmdName == "pwd" || 
+         cmdName == "cd" || cmdName == "echo" || cmdName == "debug") {
+        isValid = true;
+      }
 
       if(!isValid) {
-        cout << "Unknown command: " << input << endl;
+        cout << input << ": command not found" << endl;
         continue;
       }
     }
@@ -34,11 +47,32 @@ int main() {
     file << input << endl;
     }
     if(input == "\\q") {
-    return 0;
+      return 0;
     }
-    else if(input.find("echo ") == 0) {
-    string message = input.substr(5);
-    cout << message << endl;
+    
+    // Parse input to handle quotes for echo and debug
+    vector<string> args;
+    string current_arg;
+    bool in_quotes = false;
+    for(char c : input) {
+        if(c == '\'') {
+            in_quotes = !in_quotes;
+        } else if(c == ' ' && !in_quotes) {
+            if(!current_arg.empty()) {
+                args.push_back(current_arg);
+                current_arg.clear();
+            }
+        } else {
+            current_arg += c;
+        }
+    }
+    if(!current_arg.empty()) args.push_back(current_arg);
+
+    if(!args.empty() && (args[0] == "echo" || args[0] == "debug")) {
+        for(size_t i = 1; i < args.size(); ++i) {
+            cout << args[i] << (i == args.size() - 1 ? "" : " ");
+        }
+        cout << endl;
     } 
     else if(input == "history") {
       ifstream read_file(history_file);
